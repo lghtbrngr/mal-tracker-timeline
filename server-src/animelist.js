a5ea5b5fb8105bda4e53cf203563baeeb780f6da
Lucas Sanchez <lightbringer@lghtbrngr.com>
2023-10-08 22:31:33 -0700

    connect to MAL api and retrieve json list of currently watching anime

---
+++ server-src/animelist.js
@@ -0,0 +1,14 @@
+const malUsername = process.env.MAL_USERNAME;
+const malClientId = process.env.MAL_CLIENT_ID;
+
+exports.retrieve = async (req, res) => {
+  const url = `https://api.myanimelist.net/v2/users/${malUsername}/animelist?status=watching`;
+  const options = {
+    headers: {
+      "X-MAL-CLIENT-ID": malClientId,
+    },
+  };
+  const response = await fetch(url, options);
+  const result = await response.json();
+  res.send(result);
+};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+const animelist = require('./server-src/animelist.js');
+
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send("Hello api");
-});
+app.get('/api/animelist', animelist.retrieve);
 
 app.listen(3001, () => {
   console.log("Server running");
