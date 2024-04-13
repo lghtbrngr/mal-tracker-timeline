98a2426c892a239406fd4562e51555beef4ad6f8
Lucas Sanchez <lightbringer@lghtbrngr.com>
2024-04-13 14:55:29 -0700

    use callOnce to prevent duplicate api calls to fetch anime lists

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchAnimeList } from '../state/animeSlice';
 import Header from '../components/Header';
 import Body from '../components/Body';
+import { callOnce } from '../hooks';
 
 function App() {
   const dispatch = useDispatch();
-  useEffect(() => {
+  callOnce(() => {
     dispatch(fetchAnimeList());
-  }, []);
+  });
 
   return (
     <div className="flex flex-col h-screen">
