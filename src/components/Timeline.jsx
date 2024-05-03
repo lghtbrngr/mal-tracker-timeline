8de21f8878d9ea935e548189e0aac7bb0c17d114
Lucas Sanchez <lightbringer@lghtbrngr.com>
2024-05-03 16:07:27 -0700

    split cwlist by cutoff date

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -6,12 +6,29 @@ import { IMAGE_WIDTH } from '../constants';
 import { dayOf } from '../util';
 import { selectCwList } from '../state/animeSlice';
 
-function calculatePositions(sortedList, offset) {
-  // Calculate the horizontal and vertical positions of each anime in the sortedList
+function sortAndSplitByCutoffDate(sourceCWList) {
+  const cwList = sourceCWList.slice();
+  cwList.sort((a, b) => { // sorted by last updated date, most recent first
+    const d1 = new Date(a.list_status.updated_at);
+    const d2 = new Date(b.list_status.updated_at);
+    return d2 - d1;
+  });
+
+  // split the array in two based on the cutoff date
+  const cutoffDate = dayOf(new Date());
+  cutoffDate.setMonth(cutoffDate.getMonth() - 3);
+  const cutoffIndex = cwList.findIndex(a => new Date(a.list_status.updated_at) < cutoffDate);
+  const flushList = cutoffIndex > -1 ? cwList.splice(cutoffIndex) : [];
+
+  return [cwList, flushList];
+}
+
+function calculatePositions(cwList, offset) {
+  // Calculate the horizontal and vertical positions of each anime in the cwList
   // Assumption: list is sorted from most recent (rightmost) to least recent
   // x represents an offset in pixels; y represents a distinct channel, or row
-  if (sortedList.length === 0) return [];
-  const xOffsets = sortedList.map(anime => offset(dayOf(anime.list_status.updated_at)));
+  if (cwList.length === 0) return [];
+  const xOffsets = cwList.map(anime => offset(dayOf(anime.list_status.updated_at)));
   const positions = [{ x: xOffsets[0], y: 0 }];
   for (let i = 1; i < xOffsets.length; i += 1) {
     const x = xOffsets[i];
@@ -26,38 +43,33 @@ export default function Timeline() {
   const windowWidth = useWindowSize()[0];
   const [timelineWidth, timelineRef] = measureWidth([windowWidth]);
 
-  const cwList = useSelector(selectCwList);
+  const sourceCWList = useSelector(selectCwList);
 
-  const sortedList = cwList.slice();
-  sortedList.sort((a, b) => {
-    const d1 = new Date(a.list_status.updated_at);
-    const d2 = new Date(b.list_status.updated_at);
-    return d2 - d1;
-  });
+  const [cwList, flushList] = sortAndSplitByCutoffDate(sourceCWList);
 
   let timelineData = {};
-  if (sortedList.length > 0) {
-    const leastRecentDate = dayOf(sortedList[sortedList.length - 1].list_status.updated_at);
+  if (cwList.length > 0) {
+    const leastRecentDate = dayOf(cwList[cwList.length - 1].list_status.updated_at);
+    const timespan = dayOf(new Date()) - leastRecentDate;
+    const horizontalPadding = IMAGE_WIDTH; // don't cut off image at either side of the timeline
+    const leftPadding = horizontalPadding / 2;
     timelineData = {
       leastRecentDate,
       offset: (date) => {
-        const timespan = dayOf(new Date()) - leastRecentDate;
         const percentage = (date.valueOf() - leastRecentDate.valueOf()) / timespan;
-        const horizontalPadding = IMAGE_WIDTH; // don't cut off image at either side of the timeline
-        const leftPadding = horizontalPadding / 2;
         return percentage * (timelineWidth - horizontalPadding) + leftPadding;
       },
     };
   }
 
-  const positions = calculatePositions(sortedList, timelineData.offset);
+  const positions = calculatePositions(cwList, timelineData.offset);
 
   return (
     <div className="relative border-b-2 border-black" ref={timelineRef}>
-      {sortedList.map((anime, i) => (
+      {cwList.map((anime, i) => (
         <AnimeCard key={anime.node.id} anime={anime} position={positions[i]} />
       ))}
-      {sortedList.length > 0 && renderMonthMarkers(timelineData)}
+      {cwList.length > 0 && renderMonthMarkers(timelineData)}
     </div>
   );
 }
