6ae2453f76b54b06b7c29a599295df478f7d3d97
Lucas Sanchez <lightbringer@lghtbrngr.com>
2024-07-29 00:50:10 -0700

    remove manual vertical positioning from TitlePanel

--- src/components/TitlePanel.tsx
+++ src/components/TitlePanel.tsx
@@ -1,27 +1,19 @@
 import clsx from 'clsx';
 import { Anime } from '../types';
-import { measureHeight } from '../hooks';
 
 interface TitlePanelProps {
   anime: Anime;
 }
 
 export default function TitlePanel({ anime }: TitlePanelProps) {
-  const [height, ref] = measureHeight<HTMLDivElement>();
   const url = `https://myanimelist.net/anime/${anime.node.id}`;
   return (
     <div
       className={clsx([
-        'absolute',
-        'z-30',
         'border border-gray-400 border-b-0 bg-white',
         'p-1 text-xs overflow-hidden overflow-ellipsis',
         'w-full',
       ])}
-      style={{
-        top: `-${height}px`,
-      }}
-      ref={ref}
     >
       <a
         href={url}
