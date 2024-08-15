f8b368fa57fd118088334e430dce46e3ec4b8aa7
Lucas Sanchez <lightbringer@lghtbrngr.com>
2024-08-15 00:22:37 -0700

    use line clamp / ellipsis on long titles in AnimeCard

--- src/components/TitlePanel.tsx
+++ src/components/TitlePanel.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { Property } from 'csstype';
 import { Anime } from '../types';
 import { measureHeight } from '../hooks';
 
@@ -9,15 +10,22 @@ interface TitlePanelProps {
 export default function TitlePanel({ anime }: TitlePanelProps) {
   const [height, ref] = measureHeight<HTMLDivElement>();
   const url = `https://myanimelist.net/anime/${anime.node.id}`;
+  const multilineEllipsisStyle = {
+    display: '-webkit-box',
+    WebkitLineClamp: 2,
+    WebkitBoxOrient: 'vertical' as Property.BoxOrient,
+  };
+
   return (
     <div
       className={clsx([
         'absolute w-full z-30',
         'border border-gray-400 border-b-0 bg-white',
-        'p-1 text-xs overflow-hidden overflow-ellipsis',
+        'p-1 pb-0.5 text-xs overflow-hidden',
       ])}
       style={{
         top: `-${height}px`,
+        ...multilineEllipsisStyle,
       }}
       ref={ref}
     >
